8a35806400009847c08f7cf9a6a64f99c98f7633
Khôi <nova1x1996@gmail.com>
2023-04-09 00:40:31 +0700

    Setup Initial 2

---
+++ my_app/lib/page/favorite/favorite_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class FavoriteBody extends StatelessWidget {
+  const FavoriteBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
